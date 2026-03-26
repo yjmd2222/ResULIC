f66b6b40096f5fb955a8abc166b8445895c5e182
yjmd2222 <yjmd2222@gmail.com>
2026-03-26 18:13:56 +0900

    warnings

--- model/losses/lpips.py
+++ model/losses/lpips.py
@@ -76,7 +76,8 @@ class NetLinLayer(nn.Module):
 class vgg16(torch.nn.Module):
     def __init__(self, requires_grad=False, pretrained=True):
         super(vgg16, self).__init__()
-        vgg_pretrained_features = models.vgg16(pretrained=pretrained).features
+        weights = models.VGG16_Weights.IMAGENET1K_V1 if pretrained else None
+        vgg_pretrained_features = models.vgg16(weights=weights).features
         self.slice1 = torch.nn.Sequential()
         self.slice2 = torch.nn.Sequential()
         self.slice3 = torch.nn.Sequential()
